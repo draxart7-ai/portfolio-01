b95354f419ec191a50c9134ba4dd76605cda8b7c
draxart7 <draxart7@gmail.com>
2026-02-16 20:48:19 -0700

    added tab header icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
-import { useTheme } from "../providers/app-context"; // Use the custom hook
+import { useTheme } from "../providers/app-context";
+import "./header.css";
+import { LogoSvg } from "../assets/icons/LogoSvg";
 
 export const Header = () => {
   const { theme, toggleTheme } = useTheme();
   console.log({ theme, toggleTheme });
-  return <div>header</div>;
+  return (
+    <div className="app-header">
+      <LogoSvg height="40px" />
+      header
+    </div>
+  );
 };
